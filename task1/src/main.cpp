b65bc9e404907b7f1819ab42eced844cce274ce1
Łukasz Ziobroń <lukasz.ziobron@gmail.com>
2019-03-28 16:04:33 +0100

    Solved

--- task1/src/main.cpp
+++ task1/src/main.cpp
@@ -1,8 +1,15 @@
 #include <vector>
-
+#include <iostream>
+#include <future>
+#include <thread>
 #include <libmail/send_mail.h>
 #include <libmail/randomize_results.h>
 
+void send(const std::string &address, const std::string &message, std::promise<bool> prom)
+{
+	auto b = libmail::send_mail(address, message);
+	prom.set_value(b);
+}
 
 int main()
 {
@@ -16,5 +23,24 @@ int main()
 	};
 	std::string message = "build failed";
 
-	// ...
+	std::vector<std::promise<bool>> results{responsible_devs.size()};
+	std::vector<std::future<bool>> futures{};
+	std::vector<std::thread> threads;
+
+	for (auto && promise : results) {
+		futures.emplace_back(promise.get_future());
+	}
+
+	for (std::size_t i = 0u; i < responsible_devs.size(); i++) {
+		threads.emplace_back(send, responsible_devs[i], message, std::move(results.at(i)));
+	}
+	for (std::size_t i = 0u; i < futures.size(); i++){
+		futures[i].wait();
+		if (!futures[i].get()) {
+			std::cout << "Not delivered to " << responsible_devs[i] << std::endl;
+		}
+	}
+	for (auto && thread : threads) {
+		thread.join();
+	}
 }
--- task2/src/main.cpp
+++ task2/src/main.cpp
@@ -1,8 +1,21 @@
 #include <vector>
-
+#include <iostream>
+#include <future>
+#include <thread>
+#include <tuple>
+#include <string>
 #include <libmail/send_mail.h>
 #include <libmail/randomize_results.h>
 
+void send(const std::string &address, const std::string &message, std::promise<std::pair<bool, std::string>> prom)
+{
+	try {
+		libmail::send_mail(address, message);
+		prom.set_value(std::make_pair(true, ""));
+	} catch (const std::runtime_error & re) {
+		prom.set_value(std::make_pair(false, re.what()));
+	}
+}
 
 int main()
 {
@@ -16,5 +29,31 @@ int main()
 	};
 	std::string message = "build failed";
 
-	// ...
+	std::vector<std::promise<std::pair<bool, std::string>>> results{responsible_devs.size()};
+	std::vector<std::future<std::pair<bool, std::string>>> futures{};
+	std::vector<std::thread> threads;
+
+	for (auto &&promise : results)
+	{
+		futures.emplace_back(promise.get_future());
+	}
+
+	for (std::size_t i = 0u; i < responsible_devs.size(); i++)
+	{
+		threads.emplace_back(send, responsible_devs[i], message, std::move(results.at(i)));
+	}
+	for (std::size_t i = 0u; i < futures.size(); i++)
+	{
+		futures[i].wait();
+		auto result = futures[i].get();
+		if (!result.first)
+		{
+			std::cout << "Not delivered to " << responsible_devs[i]
+					  << ", cause: " << result.second << std::endl;
+		}
+	}
+	for (auto &&thread : threads)
+	{
+		thread.join();
+	}
 }
